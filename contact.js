288357baf1ee89ffa89e7f276012d256695df83c
alenka-rondon <85842242+alenka-rondon@users.noreply.github.com>
2022-03-12 22:04:11 -0500

    crossing fingers

---
+++ contact.js
@@ -0,0 +1,14 @@
+
+
+
+const nameInput = document.querySelector("#username");
+const emailInput = document.querySelector("#useremail");
+const subjectInput = document.querySelector("#subject");
+const messageInput = document.querySelector("#message");
+
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+
+function validateForm() {
+    console.log("HELLO WORLD!");
+}
