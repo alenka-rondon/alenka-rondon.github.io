421b90a55a981102489ceaf49d5899d6262594a8
alenka-rondon <85842242+alenka-rondon@users.noreply.github.com>
2022-03-12 21:33:03 -0500

    c

--- contact.php
+++ contact.php
@@ -1,11 +1,17 @@
 <?php
+
+if (isset($_POST['submit']) ) {
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $subject = $_POST['subject'];
     $message = $_POST['message'];
+
     $formcontent="From: $name \n Message: $message";
     $recipient = "alenka.rondon@gmail.com";
-    $subject = $_POST['subject'];;
     $mailheader = "From: $email \r\n";
+
     mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    
     echo "Thank You!";
+}
 ?>
